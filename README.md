efeabadbd85328920d3e2bbed470f67dbc2f3263
Muhammad Ijlal <mijlalawan@gmail.com>
2024-08-31 22:53:07 +0400

    Basic django app with user model overide and segerated settings module

---
+++ README.md
@@ -0,0 +1,7 @@
+# Basic Django App
+
+Template repository for fullstack development using Django
+
+The project has multiple branches each with a certain level of utilities integrated.
+* **basic:** The very basic setup with sqlite as db, and dockerization scripts.
+* **postgres:** Database updated from sqlite to postgreSQL.
---
+++ proj/settings/production.py
@@ -0,0 +1,36 @@
+from .base import *
+from .base import config
+
+DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(",")
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
